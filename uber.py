029e65191991ddac02ab7e6ace777456f1766251
arulbalu2608 <60419376+arulbalu2608@users.noreply.github.com>
2020-01-29 12:46:44 +0530

    Create uber.py

---
+++ uber.py
@@ -0,0 +1,36 @@
+print("Welcome to uber Available destinations are Ambattur,Koyambed,Royyapurram")
+print("Ambattur:amb\n Koyambed:cmbt\n Royyapurram:roy\n")
+d=raw_input("Enter your destination:")
+if(d=='amb'):
+    print("source: Avadi")
+    print("Destination:Ambattur")
+    print("Total Fare:150")
+    print("Total km:20")
+elif(d=="cmbt"):
+    print("source: Avadi")
+    print("Destination:koyambed")
+    print("Total Fare:250")
+    print("Total km:30")
+elif(d=='roy'):
+    print("source: Avadi")
+    print("Destination:Royyapurram")
+    print("Total Fare:200")
+    print("Total km:28")
+
+
+
+'''
+input:roy
+output:
+Welcome to uber Available destinations are Ambattur,Koyambed,Royyapurram
+Ambattur:amb
+ Koyambed:cmbt
+ Royyapurram:roy
+
+Enter your destination:roy
+source: Avadi
+Destination:Royyapurram
+Total Fare:200
+Total km:28
+
+'''
